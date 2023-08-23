82ebabc8a5572ef1de871899d4d2562fd7c6648b
Glory Arisah <garisah@ipnxnigeria.net>
2023-08-23 01:43:51 +0100

    removed font-awesome from project

--- src/components/TheLoader.vue
+++ src/components/TheLoader.vue
@@ -1,14 +1,8 @@
 <template>
-  <div>
-    <font-awesome-icon icon="spinner" animation="spin" />
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faSpinner);
 export default {};
 </script>
 
